21dba6614ba15b9c7e8706215d291355743a4e7b
facundo sura <facundomesura@gmail.com>
2025-11-11 01:16:51 -0300

    componente footer en el layout

--- pintureria-casco-viejo/app/page.tsx
+++ pintureria-casco-viejo/app/page.tsx
@@ -7,41 +7,7 @@ export default function Home() {
       
       <main></main>
       <aside></aside>
-      <footer className="absolute bottom-0 left-0 right-0 bg-black/5 p-2 shadow-md z-50 px-12 flex items-start justify-between">
-        <div>
-          <Image src="/logo_169.png" alt="logo" width={200} height={100} />
-          <p>La mejor pintureria del pais</p>
-        </div>
-        <ul>
-          <li className="border-b border-zinc-300 pb-1">
-            <h4>enlaces rapidos</h4>
-          </li>
-          <li>
-            <Link href="/">Inicio</Link>
-          </li>
-          <li>
-            <Link href="/products">Productos</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contacto</Link>
-          </li>
-          <li>
-            <Link href="/aboutUs">Sobre Nosotros</Link>
-          </li>
-          <li>
-            <Link href="/FAQ">Preguntas Frecuentes</Link>
-          </li>
-          <li>
-            <Link href="/terms">Terminos y Condiciones</Link>
-          </li>
-        </ul>
-        <div>
-          <h4 className="border-b border-zinc-300 pb-1">Contacto</h4>
-          <p>Email: info@pintureriacascoviejo.com</p>
-          <p>Telefono: 123-456-7890</p>
-          <p>Direccion: Calle Falsa 123, Ciudad, País</p>
-        </div>
-      </footer>
+      
     </div>
   );
 }
