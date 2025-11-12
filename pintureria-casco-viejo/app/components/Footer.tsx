09b7af7608b5f8c21f576d1163f67d0fb9801e6d
facundo sura <facundomesura@gmail.com>
2025-11-12 09:49:39 -0300

    footer responsive

--- pintureria-casco-viejo/app/components/Footer.tsx
+++ pintureria-casco-viejo/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Footer() {
     return(
-        <footer className="absolute bottom-0 left-0 right-0 bg-black/5 p-8 shadow-md z-50 px-8 flex items-start justify-between">
+        <footer className="absolute bottom-0 left-0 right-0 bg-black/5 p-8 shadow-md z-50 px-8 grid grid-cols-1 md:grid-cols-3 gap-10">
         <div>
           <Image src="/logo_169.png" alt="logo" width={200} height={100} />
           <p>La mejor pintureria del pais</p>
